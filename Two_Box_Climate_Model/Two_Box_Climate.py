bf4db8d44f3c59d4994ed315acf8fb8ff2899a18
Aodhan Sweeney <ajs15e@my.fsu.edu>
2019-02-12 10:34:41 -0800

    Box-Model Working Version

---
+++ Two_Box_Climate_Model/Two_Box_Climate.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+"""Welcome to the two box climate scheme. This code tests parameterizations of the meridional heat flux between the 
+two box system. In this climate model, two boxes share energy via a heat flux term. Because of the ambiguity 
+surrounding how to best parameterize this heat flux, this two box system has been created to test the traditional 
+linear modeling vs a new parameterization created by Sura & Sweeney 2018. To see the mathematical modeling of 
+each of the boxes, look at the two_box_climate_utils script. This code will allow you to plot the two box climate 
+in a 3d space consisting of the solar intensity for each of the boxes and the equilibrium temperatures found by the
+coupled autonomous equations representing climate."""
+
+
+#BEGIN CODE
+
+
+import nbimporter
+import numpy as np
+import two_box_climate_utils
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+get_ipython().run_line_magic('matplotlib', 'notebook')
+
+if __name__ == '__main__':
+        
+    mu1 = np.linspace(.9,1.1,100) #Variations of the solar constant paramter for polar zone
+    mu2 = np.linspace(.9,1.1,100) #Variations for the tropic solar constant
+    
+    #First we must create the linear albedo feedback solutions for both the polar and tropical zones respectively.
+    deepfreeze_solns = two_box_climate_utils.linear_albedo_solver(mu1, mu2, 250, 255)
+    deepfreeze_solns = deepfreeze_solns.astype(float)
+    
+    
+    #Begining of User Input. User will specifiy whether to plot the linear or non-linear heat flux:
+    lin_or_nonlin = str(input("Shall we plot the linear or non-linear heat flux parameterization? Type linear or non-linear: " ))
+    
+    
+    if lin_or_nonlin == 'non-linear':
+        
+        #Looking for the lower branch solution that comes from the bifurcation line.
+        solutions_100 = two_box_climate_utils.nonlinear_equilibrium(mu1, mu2, (150+1j), (155+1j))
+        solutions_100 = np.array(solutions_100)
+        solutions_100 = solutions_100.astype(float)
+        #Now finding the upper branch comming out of bifucation line
+        solutions_350 = two_box_climate_utils.nonlinear_equilibrium(mu1, mu2, (350+1j), (355+1j))
+        solutions_350 = np.array(solutions_350)
+        solutions_350 = solutions_350.astype(float)
+        #The non-linear upper branch solution is most complete with two solution sets with different guess values.
+        solutions_500 = two_box_climate_utils.nonlinear_equilibrium(mu1, mu2, (500+1j), (505+1j))
+        solutions_500 = np.array(solutions_500)
+        solutions_500 = solutions_500.astype(float)
+
+        polar_tropic_input = str(input('Shall we plot the tropical equilibrium surface or polar? Type polar or tropic: '))
+        
+        if polar_tropic_input == 'polar':
+            
+            fig = plt.figure(figsize=(15, 12))
+            ax = fig.add_subplot(111, projection='3d')          
+            
+            xs = solutions_100[:,2]
+            ys = solutions_100[:,3]
+            zs = solutions_100[:,0]
+            ax.scatter(xs, ys, zs, c='orange', alpha=0.6, edgecolors='w')
+            xs = solutions_350[:,2]
+            ys = solutions_350[:,3]
+            zs = solutions_350[:,0]
+            ax.scatter(xs, ys, zs, c='red', alpha=0.6, edgecolors='w')
+            xs = solutions_500[:,2]
+            ys = solutions_500[:,3]
+            zs = solutions_500[:,0]
+            ax.scatter(xs, ys, zs, c='red', alpha=0.6, edgecolors='w')
+            xs = deepfreeze_solns[:,2]
+            ys = deepfreeze_solns[:,3]
+            zs = deepfreeze_solns[:,0]
+            ax.scatter(xs, ys, zs, c='blue', alpha=0.6, edgecolors='w')
+
+            ax.set_xlabel('Polar mu values')
+            ax.set_ylabel('Tropical mu Values')
+            ax.set_zlabel('Polar Equilibrium Temperatures')
+            plt.title('Non-Linear Heat Flux Polar Equilibrium Temperatures')
+
+            stability_lower = two_box_climate_utils.stability_analysis(solutions_100, 'linear')
+            print("Eigenvalues for stability analysis of lower branch are ", stability_lower[0][0], " and ",
+                 stability_lower[0][1])
+            stability_upper = two_box_climate_utils.stability_analysis(solutions_350, 'linear')
+            print("For the upper branch, stability is ", stability_upper[0][0], " and ", stability_upper[0][1])
+            
+            
+        elif polar_tropic_input == 'tropic':
+            
+            fig = plt.figure(figsize=(15, 12))
+            ax = fig.add_subplot(111, projection='3d')            
+            
+            xs = solutions_100[:,2]
+            ys = solutions_100[:,3]
+            zs = solutions_100[:,1]
+            ax.scatter(xs, ys, zs, c='orange', alpha=0.6, edgecolors='w')
+            xs = solutions_350[:,2]
+            ys = solutions_350[:,3]
+            zs = solutions_350[:,1]
+            ax.scatter(xs, ys, zs, c='red', alpha=0.6, edgecolors='w')
+            xs = solutions_500[:,2]
+            ys = solutions_500[:,3]
+            zs = solutions_500[:,1]
+            ax.scatter(xs, ys, zs, c='red', alpha=0.6, edgecolors='w')
+            xs = deepfreeze_solns[:,2]
+            ys = deepfreeze_solns[:,3]
+            zs = deepfreeze_solns[:,1]
+            ax.scatter(xs, ys, zs, c='blue', alpha=0.6, edgecolors='w')
+
+            ax.set_xlabel('Polar mu values')
+            ax.set_ylabel('Tropical mu Values')
+            ax.set_zlabel('Tropical Equilibrium Temperatures')
+            plt.title('Non-Linear Heat Flux Tropical Equilibrium Temperatures')
+            
+            stability_lower = two_box_climate_utils.stability_analysis(solutions_100, 'linear')
+            print("Eigenvalues for stability analysis of lower branch are ", stability_lower[0][0], " and ",
+                 stability_lower[0][1])
+            stability_upper = two_box_climate_utils.stability_analysis(solutions_350, 'linear')
+            print("For the upper branch, stability is ", stability_upper[0][0], " and ", stability_upper[0][1])
+            
+        else:
+            print('That was not a valid input')
+
+    elif lin_or_nonlin == 'linear':
+        #Looking for the lower branch solution that comes from the bifurcation line.
+        solutions_100 = two_box_climate_utils.linear_equilibrium(mu1, mu2, (150+1j), (155+1j))
+        solutions_100 = np.array(solutions_100)
+        solutions_100 = solutions_100.astype(float)
+        
+        #Now finding the upper branch comming out of bifucation line.
+        solutions_350 = two_box_climate_utils.linear_equilibrium(mu1, mu2, (350+1j), (355+1j))
+        solutions_350 = np.array(solutions_350)
+        solutions_350 = solutions_350.astype(float)
+        polar_tropic_input = input("Shall we plot the tropical equilibrium surface or polar? Type polar or tropic: ")
+
+
+        if polar_tropic_input == 'polar':
+            
+            fig = plt.figure(figsize=(15, 12))
+            ax = fig.add_subplot(111, projection='3d')            
+            
+            xs = solutions_100[:,2]
+            ys = solutions_100[:,3]
+            zs = solutions_100[:,0]
+            ax.scatter(xs, ys, zs, c='orange', alpha=0.6, edgecolors='w')
+            xs = solutions_350[:,2]
+            ys = solutions_350[:,3]
+            zs = solutions_350[:,0]
+            ax.scatter(xs, ys, zs, c='red', alpha=0.6, edgecolors='w')
+            xs = deepfreeze_solns[:,2]
+            ys = deepfreeze_solns[:,3]
+            zs = deepfreeze_solns[:,0]
+            ax.scatter(xs, ys, zs, c='blue',alpha=0.6, edgecolors='w')
+
+            ax.set_xlabel('Polar mu values')
+            ax.set_ylabel('Tropical mu Values')
+            ax.set_zlabel('Polar Equilibrium Temperatures')
+            plt.title('Linear Heat Flux Polar Equilibrium Temperatures')
+            
+            stability_lower = two_box_climate_utils.stability_analysis(solutions_100, 'linear')
+            print("Eigenvalues for stability analysis of lower branch are ", stability_lower[0][0], " and ",
+                 stability_lower[0][1])
+            stability_upper = two_box_climate_utils.stability_analysis(solutions_350, 'linear')
+            print("For the upper branch, stability is ", stability_upper[0][0], " and ", stability_upper[0][1])
+
+        elif polar_tropic_input == 'tropic':
+            
+            fig = plt.figure(figsize=(15, 12))
+            ax = fig.add_subplot(111, projection='3d')            
+            
+            xs = solutions_100[:,2]
+            ys = solutions_100[:,3]
+            zs = solutions_100[:,1]
+            ax.scatter(xs, ys, zs, c='orange', alpha=0.6, edgecolors='w')
+            xs = solutions_350[:,2]
+            ys = solutions_350[:,3]
+            zs = solutions_350[:,1]
+            ax.scatter(xs, ys, zs, c='red', alpha=0.6, edgecolors='w')
+            xs = deepfreeze_solns[:,2]
+            ys = deepfreeze_solns[:,3]
+            zs = deepfreeze_solns[:,1]
+            ax.scatter(xs, ys, zs, c='blue', alpha=0.6, edgecolors='w')
+
+            ax.set_xlabel('Polar mu values')
+            ax.set_ylabel('Tropical mu Values')
+            ax.set_zlabel('Polar Equilibrium Temperatures')
+            plt.title('Linear Heat Flux Tropical Equilibrium Temperatures')
+            
+            stability_lower = two_box_climate_utils.stability_analysis(solutions_100, 'linear')
+            print("Eigenvalues for stability analysis of lower branch are ", stability_lower[0][0], " and ",
+                 stability_lower[0][1])
+            stability_upper = two_box_climate_utils.stability_analysis(solutions_350, 'linear')
+            print("For the upper branch, stability is ", stability_upper[0][0], " and ", stability_upper[0][1])
+            
+        else:
+            print('That was not a valid input. Run the model again.')
+
+
+# In[13]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Two_Box_Climate_Model/two_box_climate_utils.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+"""This is a tool box for the Two Box Climate Scheme modlue. In this tool box are functions used for calculating
+equilibrium position, its stability, and other quantities associated with the two box model created in the 2018-2019 
+work of Sura & Sweeney"""
+
+
+#BEGIN CODE
+
+
+def linear_equilibrium(mu1, mu2, complex_guess_1, complex_guess_2):
+    """The linear equilibrium function finds a two dimensional vector solution. The solutions we are looking for are real.
+    Many solutions that can be found are complex and because of the numerical methods in which python finds these solutions
+    we have decided to use the mpmath modules. This method of finding the solution for our non linear heat flux uses
+    the muller method of numerical calculation. For more information on mpmath root finding: 
+    http://mpmath.org/doc/1.1.0/calculus/optimization.html"""
+    import numpy as np
+    import sympy
+    import mpmath
+    #List of Constants
+    I0 = 1367.0 #Solar constant W/m^2
+    a = 2.8 #Unitless parameter needed for linear albedo feedback relationships more info in Budyko 1969
+    b=.009 #Another parameter needed for linear feedback relationship more info in Budyko 1969
+    sig = 5.67*10**-8 #Stephan boltzmann constant m^2 kg s^-2 K^-1
+    e = .64 #Emmisivity of earth
+    A = 2.7 #Heat flux parameter for non linear heat forcing
+    solution_array = []
+    for x in mu1:
+        for y in mu2:
+            #Below we find the two-dimensional vector solutions in form of [polar_solution, tropical_solution]
+            f = [lambda T1,T2: (.156*x*I0*(1-a) + .156*x*I0*b*T1 - e*sig*(T1**4)+(A*(T2 - T1))), 
+                 lambda T1,T2: (.288*y*I0*(1-a) + .288*y*I0*b*T2 - e*sig*(T2**4)+(A*(T1 - T2)))]
+            solution = np.array(mpmath.findroot(f, ([complex_guess_1, complex_guess_2]), 
+                                                solver='muller', verify = False))
+            if abs(mpmath.im(solution[0])) > 1e-10:
+                solution[0] = np.nan
+            if abs(mpmath.im(solution[1])) > 1e-10:
+                solution[1] = np.nan
+            solution_array.append([mpmath.re(solution[0]), mpmath.re(solution[1]), x, y])
+    return(solution_array)
+
+
+
+def nonlinear_equilibrium(mu1, mu2, complex_guess_1, complex_guess_2):
+    """The nonlinear equilibrium function finds a two dimensional vector solution. The solutions we are looking for are real.
+    Many solutions that can be found are complex and because of the numerical methods in which python finds these solutions
+    we have decided to use the mpmath modules. This method of finding the solution for our non linear heat flux uses
+    the muller method of numerical calculation. For more information on mpmath root finding: 
+    http://mpmath.org/doc/1.1.0/calculus/optimization.html"""
+    import numpy as np
+    import sympy
+    import mpmath
+    #List of Constants
+    I0 = 1367.0 #Solar constant W/m^2
+    a = 2.8 #Unitless parameter needed for linear albedo feedback relationships more info in Budyko 1969
+    b=.009 #Another parameter needed for linear feedback relationship more info in Budyko 1969
+    sig = 5.67*10**-8 #Stephan boltzmann constant m^2 kg s^-2 K^-1
+    e = .64 #Emmisivity of earth
+    A = 600 #Heat flux parameter for non linear heat forcing
+    solution_array = []
+    for x in mu1:
+        for y in mu2:
+            #Below we find the two-dimensional vector solutions in form of [polar_solution, tropical_solution]
+            f = [lambda T1,T2: (.156*x*I0*(1-a) + .156*x*I0*b*T1 - e*sig*(T1**4)+(A/(T2 -T1))), 
+                 lambda T1,T2: (.288*y*I0*(1-a) + .288*y*I0*b*T2 - e*sig*(T2**4)+(A/(T1 -T2)))]
+            solution = np.array(mpmath.findroot(f, ([complex_guess_1, complex_guess_2]), 
+                                                solver='muller', verify = False))
+            if abs(mpmath.im(solution[0])) > 1e-10:
+                solution[0] = np.nan
+            if abs(mpmath.im(solution[1])) > 1e-10:
+                solution[1] = np.nan
+            solution_array.append([mpmath.re(solution[0]), mpmath.re(solution[1]), x, y])
+    return(solution_array)
+
+    
+def linear_albedo_feedback(k_vector, mu1, mu2):
+    """To create the linear albedo feedback we follow the methods used in the 1978 fraedrich paper, assinging 
+    an 'a' parameter value of .75 and finding solutions to the system of differential equations.
+    The linear albedo feedback produced only real solutions. Because of this, we may take advantage of the speeds
+    of computation provided through the scipy package because we do not need to search for nonreal answers to 
+    eliminate. For more information: https://docs.scipy.org/doc/scipy/reference/optimize.html"""
+    import numpy as np
+    from scipy.optimize import fsolve
+    #List of Constants
+    I0 = 1367.0 #Solar constant W/m^2
+    a = .75 #Unitless parameter needed for linear albedo feedback relationships more info in Budyko 1969
+    sig = 5.67*10**-8 #Stephan boltzmann constant m^2 kg s^-2 K^-1
+    e = .64 #Emmisivity of earth
+    T1 = k_vector[0]
+    T2 = k_vector[1]
+    temp = np.zeros(2)
+    temp[0] = (.156*mu1*I0*(1-.75) - e*sig*(T1**4))*(1.0*10**-8)
+    temp[1] = (.288*mu2*I0*(1-.75) - e*sig*(T2**4))*(1.5*10**-8)
+    """    deepfreeze = []
+    for x in mu1:
+        for y in mu2:
+            solution = fsolve(linear_abledo_feedback, k_vector, args =(x,y))
+            deepfreeze.append([solution[0], solution[1], x, y])"""
+    return(temp)
+
+
+def linear_albedo_solver(mu1, mu2, guess_1, guess_2):
+    """For the case of unstable solutions and ranges of the plot, we must have a stable linear albedo feeback solution.
+    This solution is a modeling of a situation where solar intensity is decreased sufficiently until the earth 
+    produces enough ice that the earth will reflect sufficient radiation to keep the earth in a stable ice age."""
+    import numpy as np
+    from scipy.optimize import fsolve
+    deepfreeze = []
+    for x in mu1:
+        for y in mu2:
+            solution = fsolve(linear_albedo_feedback, [guess_1, guess_2], args =(x,y))
+            deepfreeze.append([solution[0], solution[1], x, y])
+    deepfreeze = np.array(deepfreeze)
+    return(deepfreeze)
+    
+
+    
+    
+#NEW STUFF
+
+def non_linear_stability_analysis(branch):
+    """The structure of the bifurcation fold leads to the question of stability for each of the braches. 
+    In order to analyze the stability of each of these branches we use the technique of finding stabilty used
+    in the Fraedrich 1978 paper. This is a linearization method where each of the coupled differential 
+    equations are differentiated and put into a jacobian matrix and eigenvalues are found. If eigenvalues are
+    both positive the equilibrium position is an unstable node, if both negative a stable node, if one postive 
+    and one negative it is unstable saddlepoint, if we have nonreal solutions then there can be no certainty in 
+    the stability."""
+    import numpy as np
+    import mpmath    
+    #List of Constants
+    I0 = 1367.0 # Solar constant W/m^2
+    b=.009 # Parameter needed for linear feedback relationship more info in Budyko 1969 K^(-1)
+    sig = 5.67*10**-8 # Stephan boltzmann constant m^2 kg s^-2 K^-1
+    e = .64 # Emmisivity of earth
+    A = 600 # Heat flux parameter. Derived for mixing timescales of 8-16 months and temp difference 20-30 deg K
+    select = ~np.isnan(branch[:,0])
+    branch = branch[select]
+    stability_verdicts = []
+    for eq_position in branch:
+        polartemp = eq_position[0]
+        tropicalttemp = eq_position[1]
+        pmu = eq_position[2] # Polar Mu Value: regulates solar intensity in polar box.
+        tmu = eq_position[3] # Tropical Mu Value: regulates solar intensity in tropical box.
+        J11 = (.156*pmu*I0*b - 4*e*sig*(polartemp**3) + A/((tropicaltemp - polartemp)**2)) #Jacobian position 11
+        J22 = (.288*tmu*I0*b - 4*e*sig*(tropicaltemp**3) + A/((polartemp-tropicaltemp)**2))#Jacobian 22
+        J12 = -A/((tropicaltemp - polartemp)**2)
+        J21 = -A/((polartemp - tropicaltemp)**2)
+
+        f = [lambda z: ((J11 - z)*(J22 - z)- (J12)*(J21))]
+        z1 = np.array(mpmath.findroot(f, ([150+1j]), solver='muller', verify = False))
+        z2 = np.array(mpmath.findroot(f, ([-150+1j]), solver='muller', verify = False))
+        stability_verdicts.append([z1,z2])
+        
+
+    return(stability_verdicts)
+
+
+def stability_analysis(branch, linear_or_non_linear):
+    """The structure of the bifurcation fold leads to the question of stability for each of the braches. 
+    In order to analyze the stability of each of these branches we use the technique of finding stabilty used
+    in the Fraedrich 1978 paper. This is a linearization method where each of the coupled differential 
+    equations are differentiated and put into a jacobian matrix and eigenvalues are found. If eigenvalues are
+    both positive the equilibrium position is an unstable node, if both negative a stable node, if one postive 
+    and one negative it is unstable saddlepoint, if we have nonreal solutions then there can be no certainty in 
+    the stability."""
+    import numpy as np
+    import mpmath    
+    #List of Constants
+    I0 = 1367.0 # Solar constant W/m^2
+    b=.009 # Parameter needed for linear feedback relationship more info in Budyko 1969 K^(-1)
+    sig = 5.67*10**-8 # Stephan boltzmann constant m^2 kg s^-2 K^-1
+    e = .64 # Emmisivity of earth
+    A = 600 # Heat flux parameter. Derived for mixing timescales of 8-16 months and temp difference 20-30 deg K
+    select = ~np.isnan(branch[:,0])
+    branch = branch[select]
+    stability_verdicts = []
+    
+    if linear_or_non_linear == str('non-linear'):
+        for eq_position in branch:
+            polartemp = eq_position[0]
+            tropicaltemp = eq_position[1]
+            pmu = eq_position[2] # Polar Mu Value: regulates solar intensity in polar box.
+            tmu = eq_position[3] # Tropical Mu Value: regulates solar intensity in tropical box.
+            J11 = (.156*pmu*I0*b - 4*e*sig*(polartemp**3) + A/((tropicaltemp - polartemp)**2)) #Jacobian position 11
+            J22 = (.288*tmu*I0*b - 4*e*sig*(tropicaltemp**3) + A/((polartemp-tropicaltemp)**2))#Jacobian 22
+            J12 = -A/((tropicaltemp - polartemp)**2)
+            J21 = -A/((polartemp - tropicaltemp)**2)
+
+            f = [lambda z: ((J11 - z)*(J22 - z)- (J12)*(J21))]
+            z1 = np.array(mpmath.findroot(f, ([150+1j]), solver='muller', verify = False))
+            z2 = np.array(mpmath.findroot(f, ([-150+1j]), solver='muller', verify = False))
+            stability_verdicts.append([z1,z2])
+    
+    if linear_or_non_linear == str('linear'):
+        for eq_position in branch:
+            polartemp = eq_position[0]
+            tropicaltemp = eq_position[1]
+            pmu = eq_position[2] # Polar Mu Value: regulates solar intensity in polar box.
+            tmu = eq_position[3] # Tropical Mu Value: regulates solar intensity in tropical box.
+            J11 = (.156*pmu*I0*b - 4*e*sig*(polartemp**3) - A) #Jacobian position 11
+            J22 = (.288*tmu*I0*b - 4*e*sig*(tropicaltemp**3) - A) #Jacobian 22
+            J12 = A
+            J21 = A
+
+            f = [lambda z: ((J11 - z)*(J22 - z)- (J12)*(J21))]
+            z1 = np.array(mpmath.findroot(f, ([150+1j]), solver='muller', verify = False))
+            z2 = np.array(mpmath.findroot(f, ([-150+1j]), solver='muller', verify = False))
+            stability_verdicts.append([z1,z2])
+
+    return(stability_verdicts)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
